064e8d3e3cd65082aec8d05db58499bda7999509
Doohwancho <doohwancho526@gmail.com>
2022-08-31 19:45:56 +0900

    A-1: @Test

---
+++ src/test/java/com/tdd/tddTest/basic/BasicTest.java
@@ -0,0 +1,13 @@
+package com.tdd.tddTest.basic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test; //junit5는 jupiter api 사용
+
+
+public class BasicTest {
+
+    @Test
+    public void Test(){
+        Assertions.assertTrue(1 == 1);
+    }
+}
