9e8ddec89f057a97b48d2a0561e136d425cfa571
Doohwancho <doohwancho526@gmail.com>
2022-09-02 21:53:11 +0900

    C-2 lombok 작동 확인

--- src/test/java/com/tdd/tddTest/jpa/PostsRepositoryTest.java
+++ src/test/java/com/tdd/tddTest/jpa/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.tdd.tddTest.jpa;
 
+import com.tdd.tddTest.domain.Posts;
 import com.tdd.tddTest.domain.PostsRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -18,6 +20,33 @@ public class PostsRepositoryTest {
     @DisplayName("repository 의존성 주입 테스트")
     public void postsRepository_의존성_주입_테스트(){
         assertThat(postsRepository.getClass().getName()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("lombok 작동 확인")
+    public void 롬복_빌더_작동_여부_확인() {
+        //given
+        String title = "hello";
+        String content = "hello world";
+        String author = "cho";
+
+        //when
+        Posts post = new Posts()
+                .builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        //then
+        assertAll(
+            () -> assertThat(post.getTitle()).isNotNull(),
+            () -> assertThat(post.getTitle()).isEqualTo(title),
+            () -> assertThat(post.getContent()).isNotNull(),
+            () -> assertThat(post.getContent()).isEqualTo(content),
+            () -> assertThat(post.getAuthor()).isNotNull(),
+            () -> assertThat(post.getAuthor()).isEqualTo(author)
+        );
     }
 
 }
