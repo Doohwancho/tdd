7b4ac0efbab35976fd553080e44e56563d831222
Doohwancho <doohwancho526@gmail.com>
2022-09-02 21:45:38 +0900

    C-2: refactor repository DI test code

--- src/test/java/com/tdd/tddTest/jpa/PostsRepositoryTest.java
+++ src/test/java/com/tdd/tddTest/jpa/PostsRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.tdd.tddTest.jpa;
 
 import com.tdd.tddTest.domain.PostsRepository;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -13,7 +15,9 @@ public class PostsRepositoryTest {
     PostsRepository postsRepository;
 
     @Test
+    @DisplayName("repository 의존성 주입 테스트")
     public void postsRepository_의존성_주입_테스트(){
-        Assertions.assertTrue(!postsRepository.getClass().getName().isEmpty());
+        assertThat(postsRepository.getClass().getName()).isNotNull();
     }
+
 }
